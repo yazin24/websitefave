69141277849eb586c26fe6f58bf71ac8c65d47b1
yazin24 <yazinconiendo@gmail.com>
2023-07-29 18:00:08 +0800

    ..

---
+++ src/components/Appointmentform.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+const Appointmentform = () => {
+    return (
+        <div className='flex flex-col items-center'>
+            <div className='bg-violet-700 w-full h-32 md:h-52'>
+            </div>
+            <div>
+                <section className="bg-gray-100">
+                    <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+                        <div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
+                            <div className="lg:col-span-2 lg:py-12">
+                                <p className="max-w-xl text-lg">
+                                    At the same time, the fact that we are wholly owned and totally
+                                    independent from manufacturer and other group control gives you
+                                    confidence that we will only recommend what is right for you.
+                                </p>
+
+                                <div className="mt-8">
+                                    <a href="" className="text-2xl font-bold text-pink-600">
+                                        +232-349-0000
+                                    </a>
+
+                                    <address className="mt-2 not-italic">
+                                        Fave Ecommerce Ph
+                                    </address>
+                                </div>
+                            </div>
+
+                            <div className="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
+                                <form action="" className="space-y-4">
+                                    <div>
+                                        <label className="sr-only" for="name">Name</label>
+                                        <input className="w-full rounded-lg border border-gray-200 p-3 text-sm" placeholder="Name" type="text" id="name"/>
+                                    </div>
+
+                                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                                        <div>
+                                            <label className="sr-only" for="email">Email</label>
+                                            <input className="w-full rounded-lg border border-gray-200 p-3 text-sm" placeholder="Email address" type="email" id="email"/>
+                                        </div>
+
+                                        <div>
+                                            <label className="sr-only" for="phone">Phone</label>
+                                            <input className="w-full rounded-lg border border-gray-200 p-3 text-sm" placeholder="Phone Number" type="tel" id="phone"/>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <label className="sr-only" for="message">Message</label>
+
+                                        <textarea
+                                            className="w-full rounded-lg border border-gray-200 p-3 text-sm" placeholder="Message" rows="8" id="message">
+
+                                         </textarea>
+                                    </div>
+
+                                    <div className="mt-4">
+                                        <button type="submit" className="inline-block w-full rounded-lg bg-violet-700 px-5 py-3 font-medium text-white sm:w-auto">
+                                            Set Appointment
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default Appointmentform
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Reviews from '../components/Reviews';
-import { FaCheckCircle,FaExchangeAlt, FaMagic, FaHandsWash, FaTree, FaRegArrowAltCircleRight } from "react-icons/fa";
+import Appointmentform from '../components/Appointmentform';
+import { FaCheckCircle, FaExchangeAlt, FaMagic, FaHandsWash, FaTree, FaRegArrowAltCircleRight } from "react-icons/fa";
 import { GiFragrance } from "react-icons/gi";
 
 const Home = () => {
@@ -37,7 +38,7 @@ const Home = () => {
 
       <div className='flex flex-justify-center mt-24'>
         <div className=''>
-        <img className='w-1/2' src="/cleaning.jpg" alt="Logo" />
+          <img className='w-1/2' src="/cleaning.jpg" alt="Logo" />
         </div>
         <div className='bg-violet-400'>
           <h2>Why Choose Us?</h2>
@@ -46,117 +47,122 @@ const Home = () => {
 
       <div className='flex flex-col mt-8 w-full mb-8'>
 
-      <div className='flex flex-col md:flex-row w-full mb-4'>
-        <div className='bg-violet-700 w-full p-4 m-4 flex flex-col rounded-md shadow-md'>
-          <span className='flex flex-row items-center'>
-          <span><FaCheckCircle className='text-5xl text-gray-100'/></span>
-          <span className='ml-2 text-2xl text-gray-200 font-bold'>Safety</span>
-          </span>
-          <span>
-            <p className='text-gray-200 font-bold'>Safety is paramount, especially when it comes to household cleaning products. We offer a good cleaning products that are safe to use around people, pets, and the environment. It has a clear usage instructions and precautions to avoid any potential hazards.</p>
-          </span>
+        <div className='flex flex-col md:flex-row justify-center w-full mb-4'>
+
+          <div className='bg-violet-700 w-full p-4 flex flex-col rounded-md shadow-md my-4'>
+            <span className='flex flex-row items-center'>
+              <span><FaCheckCircle className='text-5xl text-gray-100' /></span>
+              <span className=' text-2xl text-gray-200 font-bold'>Safety</span>
+            </span>
+            <span>
+              <p className='text-gray-200 '>Safety is paramount, especially when it comes to household cleaning products. We offer a good cleaning products that are safe to use around people, pets, and the environment. It has a clear usage instructions and precautions to avoid any potential hazards.</p>
+            </span>
           </div>
 
-        <div className='bg-violet-700 w-full p-4 m-4 flex flex-col rounded-md shadow-md'>
-        <span className='flex flex-row items-center'>
-          <span><FaExchangeAlt className='text-5xl text-gray-100'/></span>
-          <span className='ml-2 text-2xl text-gray-200 font-bold'>Versatility</span>
-          </span>
-          <span>
-            <p className='text-gray-200 font-bold'>Our cleaning products can handle multiple cleaning tasks, reducing the need for numerous specialized products. It is easy and can be used on various surfaces or materials provide convenience and cost-effectiveness.</p>
-          </span>
-        </div>
+          <div className='bg-violet-700 w-full p-4  flex flex-col rounded-md shadow-md my-4'>
+            <span className='flex flex-row items-center'>
+              <span><FaExchangeAlt className='text-5xl text-gray-100' /></span>
+              <span className=' text-2xl text-gray-200 font-bold'>Versatility</span>
+            </span>
+            <span>
+              <p className='text-gray-200 '>Our cleaning products can handle multiple cleaning tasks, reducing the need for numerous specialized products. It is easy and can be used on various surfaces or materials provide convenience and cost-effectiveness.</p>
+            </span>
+          </div>
 
-        <div className='bg-violet-700 w-full p-4 m-4 flex flex-col rounded-md shadow-md'>
-        <span className='flex flex-row items-center'>
-          <span><FaMagic className='text-5xl text-gray-100'/></span>
-          <span className='ml-2 text-2xl text-gray-200 font-bold'>Effectiveness</span>
-          </span>
-          <span>
-            <p className='text-gray-200 font-bold'>Our cleaning products are effective at removing dirt, grime, stains, and other contaminants from various surfaces. It can be use to clean efficiently and leave the area or item looking and feeling clean.</p>
-          </span>
-        </div>
+          <div className='bg-violet-700 w-full p-4  flex flex-col rounded-md shadow-md my-4'>
+            <span className='flex flex-row items-center'>
+              <span><FaMagic className='text-5xl text-gray-100' /></span>
+              <span className=' text-2xl text-gray-200 font-bold'>Effectiveness</span>
+            </span>
+            <span>
+              <p className='text-gray-200 '>Our cleaning products are effective at removing dirt, grime, stains, and other contaminants from various surfaces. It can be use to clean efficiently and leave the area or item looking and feeling clean.</p>
+            </span>
+          </div>
         </div>
 
         <div className='flex flex-col md:flex-row w-full'>
-        <div className='bg-violet-700 w-full p-4 m-4 flex flex-col rounded-md shadow-md'>
-        <span className='flex flex-row items-center'>
-          <span><FaHandsWash className='text-5xl text-gray-100'/></span>
-          <span className='ml-2 text-2xl text-gray-200 font-bold'>Ease Of Use</span>
-          </span>
-          <span>
-            <p className='text-gray-200 font-bold'>Our cleaning products are designed with user-friendliness in mind, ensuring that whether they come in spray, liquid, powder, or wipe form, the cleaning process demand minimal effort from the user. It is straightforward to use and is efficient in achieving cleaning goals.</p>
-          </span>
-        </div>
-        <div className='bg-violet-700 w-full p-4 m-4 flex flex-col rounded-md shadow-md'>
-        <span className='flex flex-row items-center'>
-          <span><GiFragrance className='text-5xl text-gray-100'/></span>
-          <span className='ml-2 text-2xl text-gray-200 font-bold'>Fragrance And Residue</span>
-          </span>
-          <span>
-            <p className='text-gray-200 font-bold'>Our good cleaning products leaves a pleasant fragrance after cleaning. Additionally, it doesnt leave behind any harmful residues that might attract dirt or cause long-term damage to the surface.</p>
-          </span>
-        </div>
-        <div className='bg-violet-700 w-full p-4 m-4 flex flex-col rounded-md shadow-md'>
-        <span className='flex flex-row items-center'>
-          <span><FaTree className='text-5xl text-gray-100'/></span>
-          <span className='ml-2 text-2xl text-gray-200 font-bold'>Environment Friendly</span>
-          </span>
-          <span>
-            <p className='text-gray-200 font-bold'>With increasing environmental concerns, choosing our cleaning products that are eco-friendly and biodegradable is essential. These products have a lower impact on the environment during manufacturing, usage, and disposal.</p>
-          </span>
-        </div>
+          <div className='bg-violet-700 w-full p-4  flex flex-col rounded-md shadow-md my-4'>
+            <span className='flex flex-row items-center'>
+              <span><FaHandsWash className='text-5xl text-gray-100' /></span>
+              <span className=' text-2xl text-gray-200 font-bold'>Ease Of Use</span>
+            </span>
+            <span>
+              <p className='text-gray-200 '>Our cleaning products are designed with user-friendliness in mind, ensuring that whether they come in spray, liquid, powder, or wipe form, the cleaning process demand minimal effort from the user. It is straightforward to use and is efficient in achieving cleaning goals.</p>
+            </span>
+          </div>
+          <div className='bg-violet-700 w-full p-4  flex flex-col rounded-md shadow-md my-4'>
+            <span className='flex flex-row items-center'>
+              <span><GiFragrance className='text-5xl text-gray-100' /></span>
+              <span className=' text-2xl text-gray-200 font-bold'>Fragrance And Residue</span>
+            </span>
+            <span>
+              <p className='text-gray-200 '>Our good cleaning products leaves a pleasant fragrance after cleaning. Additionally, it doesnt leave behind any harmful residues that might attract dirt or cause long-term damage to the surface.</p>
+            </span>
+          </div>
+          <div className='bg-violet-700 w-full p-4  flex flex-col rounded-md shadow-md my-4'>
+            <span className='flex flex-row items-center'>
+              <span><FaTree className='text-5xl text-gray-100' /></span>
+              <span className=' text-2xl text-gray-200 '>Environment Friendly</span>
+            </span>
+            <span>
+              <p className='text-gray-200 '>With increasing environmental concerns, choosing our cleaning products that are eco-friendly and biodegradable is essential. These products have a lower impact on the environment during manufacturing, usage, and disposal.</p>
+            </span>
+          </div>
         </div>
       </div>
 
       <div className='md:my-24 md:mx-8 mx-8'>
-      <Reviews/>
+        <Reviews />
       </div>
 
       <h2 className='md:mx-8 mx-8 underline text-xl'>Want a Business? Lets talk!</h2>
 
       <div className='flex flex-col md:flex-row my-4 md:my-8 md:mx-8 mx-4'>
-        
-      <div className='w-full md:w-1/4'>
-        <span className='flex flex-row md:flex-row'>
-        <FaRegArrowAltCircleRight className='text-5xl text-violet-700'/>
-        <h3 className='text-xl mt-4 ml-2'>Setup a Schedule</h3>
-        </span>
-       <p className='mt-2 md: mt-4'>Fill up the form below and wait for our cleaning experts to call or contact you</p>
-       <br/>
-       <hr/>
-       <br/>
+
+        <div className='w-full md:w-1/4'>
+          <span className='flex flex-row md:flex-row'>
+            <FaRegArrowAltCircleRight className='text-5xl text-violet-700' />
+            <h3 className='text-xl mt-4 ml-2'>Setup a Schedule</h3>
+          </span>
+          <p className='mt-2 md: mt-4'>Fill up the form below and wait for our cleaning experts to call or contact you</p>
+          <br />
+          <hr />
+          <br />
         </div>
         <div className='w-full md:w-1/4'>
-        <span className='flex flex-row md:flex-row'>
-        <FaRegArrowAltCircleRight className='text-5xl text-violet-700'/>
-        <h3 className='text-xl mt-4 ml-2'>Talk with us</h3>
-        </span>
-       <p className='mt-2 md: mt-4'>Our cleaning experts and agents will talk to you about your inquiries in starting a business with us</p>
-       <br/>
-       <hr/>
-       <br/>
+          <span className='flex flex-row md:flex-row'>
+            <FaRegArrowAltCircleRight className='text-5xl text-violet-700' />
+            <h3 className='text-xl mt-4 ml-2'>Talk with us</h3>
+          </span>
+          <p className='mt-2 md: mt-4'>Our cleaning experts and agents will talk to you about your inquiries in starting a business with us</p>
+          <br />
+          <hr />
+          <br />
         </div>
-      <div className='w-full md:w-1/4'>
-      <span className='flex flex-row md:flex-row'>
-        <FaRegArrowAltCircleRight className='text-5xl text-violet-700'/>
-        <h3 className='text-xl mt-4 ml-2'>Visit our office</h3>
-        </span>
-       <p className='mt-2 md: mt-4'> Our cleaning experts and agents will talk to you about your inquiries in starting a business with us</p>
-       <br/>
-       <hr/>
-       <br/>
+        <div className='w-full md:w-1/4'>
+          <span className='flex flex-row md:flex-row'>
+            <FaRegArrowAltCircleRight className='text-5xl text-violet-700' />
+            <h3 className='text-xl mt-4 ml-2'>Visit our office</h3>
+          </span>
+          <p className='mt-2 md: mt-4'> Our cleaning experts and agents will talk to you about your inquiries in starting a business with us</p>
+          <br />
+          <hr />
+          <br />
+        </div>
+        <div className='w-full md:w-1/4'>
+          <span className='flex flex-row md:flex-row'>
+            <FaRegArrowAltCircleRight className='text-5xl text-violet-700' />
+            <h3 className='text-xl mt-4 ml-2'>Run your business</h3>
+          </span>
+          <p className='mt-2 md: mt-4'>Our cleaning experts and agents will talk to you about your inquiries in starting a business with us</p>
+          <br />
+          <hr />
+          <br />
+        </div>
       </div>
-      <div className='w-full md:w-1/4'>
-      <span className='flex flex-row md:flex-row'>
-        <FaRegArrowAltCircleRight className='text-5xl text-violet-700'/>
-        <h3 className='text-xl mt-4 ml-2'>Run your business</h3>
-        </span>
-       <p className='mt-2 md: mt-4'>Our cleaning experts and agents will talk to you about your inquiries in starting a business with us</p>
-       <br/>
-       <hr/>
-       <br/>
-      </div>
+
+      <div>
+      <Appointmentform/>
       </div>
 
     </div>
