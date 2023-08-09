ff3ccdf7365603f51e028b27c5d78f3f07f9725e
earljay06 <118975756+earljay06@users.noreply.github.com>
2023-08-09 16:44:06 +0800

    Update Footerbar.jsx

--- src/components/Footerbar.jsx
+++ src/components/Footerbar.jsx
@@ -1,44 +1,57 @@
-import React from 'react'
-import { FaFacebook, FaYoutube, FaTiktok,FaEnvelope } from "react-icons/fa";
+import React from "react";
+import {
+  FaFacebookSquare,
+  FaTiktok,
+  FaYoutube,
+  FaEnvelope,
+} from "react-icons/fa";
 
 const Footerbar = () => {
   return (
-    
+    <footer className="bg-gradient-to-l from-violet-900 via-violet-400 to-blue-100">
+      <div className="mb-8 px-8 bg-gradient-to-l from-violet-900 via-violet-400 to-blue-100 ">
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
 
-   
-<footer className="bg-gradient-to-l from-violet-900 via-violet-400 to-blue-100">
-               
-      <div className='mb-8 px-8 bg-gradient-to-l from-violet-900 via-violet-400 to-blue-100'>
-      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Fave-Ecommerce.Ph™</a>. All Rights Reserved.
-          </span>
-          <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
-              <a href="https://www.facebook.com/people/Fave-Ecommerce-Inc/100094725815233/" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                  <FaFacebook className='text-blue-500'/>
-                  <span className="sr-only">Facebook page</span>
-              </a>
-              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                 <FaYoutube className='text-red-700'/>
-                  <span className="sr-only">Youtube channel</span>
-              </a>
-              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-              <FaTiktok className='text-gray-800'/>
-                  <span className="sr-only">Tiktok</span>
-              </a>
-              <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                  <FaEnvelope className='text-gray-200'/>
-                  <span className="sr-only">Gmail</span>
-              </a>
-        
+        <div className="flex md:space-x-14 md:flex-row justify-between mx-8 grid grid-cols-1 md:grid-cols-3">
+          <div className="mb-4 md:mb-0">
+            <img src="newlogo.png" alt="" className="h-24 w-22" />
           </div>
+
+          <div className="flex flex-col">
+            <p className="font-semibold text-white">Fave Ecommerce INC.</p>
+            <p className="font-semibold text-white">
+              8 Calle Industria Bagumbayan, Quezon City
+            </p>
+            <p className="font-semibold text-white">625-916-359-000</p>
+            <p className="font-semibold text-white">0998-887-3878</p>
+            <p className="font-semibold text-white">faveecommerce@gmail.com</p>
+          </div>
+
+          <div className="mt-8 md:mt-12 flex flex-row sm:flex-row py-4 mx-auto items-center justify-end space-x-4">
+            <div className="">
+              <FaFacebookSquare className="h-8 w-8 text-white" />
+            </div>
+
+            <div className="">
+              <FaTiktok className="h-8 w-8 text-white" />
+            </div>
+
+            <div className="">
+              <FaYoutube className="h-8 w-8 text-white" />
+            </div>
+
+            <div className="">
+              <FaEnvelope className="h-8 w-8 text-white" />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-</footer>
+    </footer>
+  );
+};
+
+export default Footerbar;
 
 
-
-  )
-}
-
-export default Footerbar
+// flex flex-column md:flex-row justify-between mx-8
+// grid grid-cols-1 sm:grid-cols-3 gap-4 lg:grid-cols-4 sm:px-8
