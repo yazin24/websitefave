fdd62d764fa1747f8ad8583ae479e0c117362aa6
earljay06 <118975756+earljay06@users.noreply.github.com>
2023-08-24 09:57:02 +0800

    Update Videoplayer.jsx

--- src/components/Videoplayer.jsx
+++ src/components/Videoplayer.jsx
@@ -8,34 +8,34 @@ const Videoplayer = () => {
     <div className="colorlay flex flex-col-reverse md:flex-row md:items-center">
       <div className="mt-4 md:mt-0 md:pr-6 md:ml-12 md:w-1/2">
         <div className="md:ml-10 flex flex-col items-center justify-center pb-10">
-{/*           <img src="newlogo.png" className="h-20 w-40" alt="" /> */}
+          {/* <img src="newlogo.png" className="h-20 w-40" alt="" /> */}
           <div className="mt-10 pl-10 flex flex-col gap-3 text-center">
             <div className="flex justify-start items-center gap-3  text-gray-200 font-bold italic">
               <FaCheck className="text-2xl" />
               <p>Grease cutting power</p>
             </div>
 
-            <div className="flex justify-start align-items gap-3 text-gray-200 font-bold italic">
+            <div className="flex justify-start items-center gap-3 text-gray-200 font-bold italic">
               <FaCheck className="text-2xl" />
               <p>Residue free clean</p>
             </div>
 
-            <div className="flex justify-start align-items gap-3 text-gray-200 font-bold italic">
+            <div className="flex justify-start items-center gap-3 text-gray-200 font-bold italic">
               <FaCheck className="text-2xl" />
               <p>Eliminate Odor</p>
             </div>
 
-            <div className="flex justify-start align-items gap-3 text-gray-200 font-bold italic">
+            <div className="flex justify-start items-center gap-3 text-gray-200 font-bold italic">
               <FaCheck className="text-2xl" />
               <p>2x Antibacterial Power</p>
             </div>
 
-            <div className="flex justify-start align-items gap-3  text-gray-200 font-bold italic">
+            <div className="flex justify-start items-center gap-3  text-gray-200 font-bold italic">
               <FaCheck className="text-2xl" />
               <p>pH Balance (5.5-6.0)</p>
             </div>
 
-            <div className="flex justify-start align-items gap-3 text-gray-200 font-bold italic">
+            <div className="flex justify-start items-center gap-3 text-gray-200 font-bold italic">
               <FaCheck className="text-2xl" />
               <p>with Moitsturizers for softer hands</p>
             </div>
@@ -49,5 +49,3 @@ const Videoplayer = () => {
 };
 
 export default Videoplayer;
-
-// mt-4 md:mt-0 md:pr-52
